361b482ded4992d4012a1fc255821b22e29ae7df
Ian <lsouza.ian@gmail.com>
2022-02-23 16:22:38 -0300

    update UI + add copy button

---
+++ README.md
@@ -0,0 +1,14 @@
+# Email Template | Email Generator
+
+A highly customizable general email generator in plain JavaScript with autofilling fields and automated copy button. (With some friendly UI)
+
+<hr>
+
+Easy-to-alter features:
+- Text fields
+- Drop downs (multiple)
+- Real time changes
+
+<hr>
+
+This is a very general email/text generator for anyone (including me) who might need a quick solution for autofilling email templates in a support-related position
--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 /* INPUTS */
 const engNameInput = document.getElementById('eng-name');
-const alertIdInput = document.getElementById('alert-id');
 const merNameInput = document.getElementById('mer-name');
 const merIdInput = document.getElementById('mer-id');
 const csmNameInput = document.getElementById('csm-name');
-const issueDescInput = document.getElementById('issue-desc');
-const impactInput = document.getElementById('impact');
-const howResolveInput = document.getElementById('how-resolve');
+let issueDescInput = document.getElementById('issue-desc');
+let impactInput = document.getElementById('impact');
+let howResolveInput = document.getElementById('how-resolve');
 
 /* SPANS - updating mid-text */
 const idSpan = document.getElementById('span-id');
@@ -15,34 +14,73 @@ const csmNameSpan = document.getElementById('span-csm-name');
 const engNameSpan = document.getElementById('span-eng-name');
 const merNameSpan = document.getElementById('span-mer-name');
 const merIdSpan = document.getElementById('span-mer-id');
-const issueDescSpan = document.getElementById("span-issue-desc");
-const impactSpan = document.getElementById("span-impact");
-const howResolveSpan = document.getElementById("span-how-resolve");
+const issueDescSpan = document.getElementById('span-issue-desc');
+const impactSpan = document.getElementById('span-impact');
+const howResolveSpan = document.getElementById('span-how-resolve');
 
 /* OUTPUTS */
-const logId = () => idSpan.innerText = alertIdInput.value;
-const logMerchant = () => merchantSpan.innerText = merNameInput.value;
-alertIdInput.addEventListener('keyup', logId);
+const logMerchant = () => (merchantSpan.innerText = merNameInput.value);
 merNameInput.addEventListener('keyup', logMerchant);
 
-const logCsm = () => csmNameSpan.innerText = csmNameInput.value;
+const logCsm = () => (csmNameSpan.innerText = csmNameInput.value);
 csmNameInput.addEventListener('keyup', logCsm);
 
-const logEng = () => engNameSpan.innerText = engNameInput.value;
+const logEng = () => (engNameSpan.innerText = engNameInput.value);
 engNameInput.addEventListener('keyup', logEng);
 
-const logMerName = () => merNameSpan.innerText = merNameInput.value;
+const logMerName = () => (merNameSpan.innerText = merNameInput.value);
 merNameInput.addEventListener('keyup', logMerName);
 
-const logMerId = () => merIdSpan.innerText = merIdInput.value;
+const logMerId = () => (merIdSpan.innerText = merIdInput.value);
 merIdInput.addEventListener('keyup', logMerId);
 
-const logIssueDesc = () => issueDescSpan.innerText = issueDescInput.value;
+const logIssueDesc = () => (issueDescSpan.innerText = issueDescInput.value);
 issueDescInput.addEventListener('keyup', logIssueDesc);
 
-const logImpact = () => impactSpan.innerText = impactInput.value;
+const logImpact = () => (impactSpan.innerText = impactInput.value);
 impactInput.addEventListener('keyup', logImpact);
 
-const logResolve = () => howResolveSpan.innerText = howResolveInput.value;
+const logResolve = () => (howResolveSpan.innerText = howResolveInput.value);
 howResolveInput.addEventListener('keyup', logResolve);
 
+const copyPopUp = document.querySelector('.copy-popup');
+
+let alertType, apiType, impactType, solutionType;
+
+const selectAlertType = sel => alertType = sel.options[sel.selectedIndex].text;
+const selectApiType = sel => apiType = sel.options[sel.selectedIndex].text;
+const selectImpactType = sel => impactType = sel.options[sel.selectedIndex].text;
+const selectResolutionType = sel => solutionType = sel.options[sel.selectedIndex].text;
+
+function applyDescChanges() {
+  issueDescInput.innerText = `There's a ${alertType} on the ${apiType} API`;
+  logIssueDesc();
+}
+
+function applyImpactChanges() {
+  impactInput.innerText = impactType;
+  logImpact();
+}
+
+function applyResolutionChanges() {
+  howResolveInput.innerText = `Verify if the merchant is facing ${solutionType.toLowerCase()} issues`;
+  logResolve();
+}
+
+/* Copy email icon */
+function copyText(elemId) {
+  let r = document.createRange();
+  r.selectNode(document.getElementById(elemId));
+  window.getSelection().removeAllRanges();
+  window.getSelection().addRange(r);
+  document.execCommand("copy");
+  window.getSelection().removeAllRanges();
+
+  setTimeout(() => {
+    copyPopUp.style.display = "none";
+  }, 1500);
+  setTimeout(() => {
+    copyPopUp.style.display = "unset";
+    document.getElementById(elemId).appendChild(copyPopUp);
+  }, 100);
+}
